2ac0da6b0ba65f3437394428b3fcbee6d2830623
José Manuel <jose.valdivia.romero@ucsp.edu.pe>
2015-06-26 00:35:25 -0500

    modified mainScreen

---
+++ Assets/VerifyMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerifyMovement : MonoBehaviour {
+
+	
+	public GameObject sourceObject;
+	private Vector3 oldPosition;
+	private Vector3 translateVector = new Vector3 (10.0f, 0.0f , 0.0f);
+
+	// Use this for initialization
+	void Start () {
+		oldPosition = sourceObject.transform.position;
+		translateVector = oldPosition + translateVector;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = sourceObject.transform.position;
+		if (oldPosition.x < sourceObject.transform.position.x)
+		{
+
+			oldPosition = temp;
+			print("right");
+		}
+		else if (oldPosition.x > sourceObject.transform.position.x)
+		{
+			oldPosition = temp;
+			print ("left");
+		}
+
+
+	}
+}
