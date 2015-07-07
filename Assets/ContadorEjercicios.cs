20c9b66b804b06c9e66d495422cdd833b451a70f
Jose E. Carrillo <quique0194@gmail.com>
2015-07-07 07:12:58 -0500

    Contador de ejercicios

---
+++ Assets/ContadorEjercicios.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ContadorEjercicios : MonoBehaviour {
+	public int cur = 0;
+	public int total = 7;
+	public int serie = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void setText () {
+		if (cur > total) {
+			cur = 0;
+			serie++;
+		}
+		Text t = gameObject.GetComponent<Text> ();
+		t.text = "Serie " + serie + "\n" + cur + "/" + total;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		setText ();
+	}
+}
