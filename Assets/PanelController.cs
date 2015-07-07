b1c011458f70e7a144ae9cc3f727095002f7dd4f
José Manuel <jose.valdivia.romero@ucsp.edu.pe>
2015-07-07 11:44:40 -0500

    Changes in Interface

---
+++ Assets/PanelController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelController : MonoBehaviour { 
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad > 4.0) {
+			transform.Translate(0, 70 * Time.deltaTime, 0);
+		}
+	}
+}
